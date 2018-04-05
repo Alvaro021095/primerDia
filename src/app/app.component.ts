416f7b1ffa1916e538340419fa3cc282309dcda0
Alvaro <lotero021095@gmail.com>
2018-04-05 14:35:27 -0500

    pruebaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Application';
+  author = 'Juan Herrera';
+  count = 0;
+  imgSource = './../favicon.ico';
+  habilitado = false;
+  show = false;
+  students = ['Alvaro', 'Kevin'];
+  people = ['Juan', 'Kevin', 'Cristian', 'Diego'];
+
+  habilitar() {
+    this.habilitado = !this.habilitado;
+  }
 }
